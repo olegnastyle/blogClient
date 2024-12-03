56177a42888fb5fffc20a828f01c6ea79c549b30
Oleg Nurzhanov <oleg@nastyle.pro>
2024-12-03 18:53:39 +0300

    *

--- components/DarkMode.vue
+++ components/DarkMode.vue
@@ -1,14 +1,14 @@
 <template>
-    <!-- <button @click=toggleTheme id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
+    <button @click=toggleTheme id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
         <svg v-if="!isDark" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
         </svg>
         <svg v-else class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
         </svg>
-    </button> -->
+    </button>
 
-    <button @click="toggleTheme" type="button">Лимпопо</button>
+    <!-- <button @click="toggleTheme" type="button">Лимпопо</button> -->
 </template>
 
 <script setup>
@@ -16,42 +16,42 @@
 //     console.log('fefefwe');
 // }
 
-function toggleTheme() {
-    console.log('fefefwe');
-}
+// function toggleTheme() {
+//     console.log('fefefwe');
+// }
 
-// const isDarkMode = ref('')
-// const isDark = ref(false)
+const isDarkMode = ref('')
+const isDark = ref(false)
 
-// onMounted(() => {
-//     if (localStorage.getItem('color-theme') === 'dark') {
-//         isDark.value = true;
-//         isDarkMode.value = 'dark'
+onMounted(() => {
+    if (localStorage.getItem('color-theme') === 'dark') {
+        isDark.value = true;
+        isDarkMode.value = 'dark'
         
         
-//     } else {
-//         isDark.value = false;
-//         isDarkMode.value = ''
-//     }
-// });
+    } else {
+        isDark.value = false;
+        isDarkMode.value = ''
+    }
+});
 
-// const toggleTheme = () => {
-//     isDark.value = !isDark.value;
-//     console.log(isDark.value);
-//     if (isDark.value) {
-//         document.documentElement.classList.add('dark');
-//         localStorage.setItem('color-theme', 'dark');
-//         console.log(isDark.value);
-//     } else {
-//         document.documentElement.classList.remove('dark');
-//         localStorage.setItem('color-theme', 'light');
-//         console.log(isDark.value);
-//     }
-// };
+const toggleTheme = () => {
+    isDark.value = !isDark.value;
+    console.log(isDark.value);
+    if (isDark.value) {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+        console.log(isDark.value);
+    } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+        console.log(isDark.value);
+    }
+};
 
-// useHead({
-//     htmlAttrs: {
-//         class: isDarkMode.value
-//     }
-// })
+useHead({
+    htmlAttrs: {
+        class: isDarkMode.value
+    }
+})
 </script>
