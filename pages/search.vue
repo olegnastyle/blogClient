b1f707c699ec3cb079c948796b76354b0ba0a68f
Oleg Nurzhanov <oleg@nastyle.pro>
2024-12-05 21:27:28 +0300

    ауау

--- pages/search.vue
+++ pages/search.vue
@@ -2,7 +2,7 @@
     <h1>Поиск</h1>
     <main>
         <article v-for="post in posts.value" :key="post.id">
-            <h3>{{ post.title }}</h3>
+            <h3>{{ post }}</h3>
         </article>
         <p v-if="posts.length === 0">Нет доступных статей.</p>
     </main>
