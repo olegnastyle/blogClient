d951a6b0f3a68e2d7bd70e18a5c67451c9007c8d
Oleg Nurzhanov <oleg@nastyle.pro>
2024-12-05 20:29:43 +0300

    pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,20 @@ export default defineNuxtConfig({
   devServer: {
     host: '0.0.0.0',
   },
+
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+
+  modules: ['@pinia/nuxt'],
+  pinia: {
+    storesDirs: ['./stores/**'],
   },
 })
