9ec41b2e7a4112c1d00efd42b5c0334dfd604906
Oleg Nurzhanov <oleg@nastyle.pro>
2024-12-05 20:58:26 +0300

    а

--- pages/search.vue
+++ pages/search.vue
@@ -14,8 +14,18 @@ function searcAlert() {
     console.log(searchResponse);
 }
 
-const response = await $fetch(`https://a1c537287dd6.vps.myjino.ru/api/posts?filters[$or][0][title][$containsi]=${search.searchQuery}&filters[$or][1][body][$containsi]=${search.searchQuery}`)
-const searchResponse = response.data
+// const response = await $fetch(`https://a1c537287dd6.vps.myjino.ru/api/posts?filters[$or][0][title][$containsi]=${search.searchQuery}&filters[$or][1][body][$containsi]=${search.searchQuery}`)
+// const searchResponse = response.data
 
+async function searchAlert() {
+    console.log(search.searchQuery);
 
+    try {
+        const response = await $fetch(`https://a1c537287dd6.vps.myjino.ru/api/posts?filters[$or][0][title][$containsi]=${search.searchQuery}&filters[$or][1][body][$containsi]=${search.searchQuery}`);
+        const searchResponse = response.data;
+        console.log(searchResponse);
+    } catch (error) {
+        console.error('Ошибка при выполнении запроса:', error);
+    }
+}
 </script>
