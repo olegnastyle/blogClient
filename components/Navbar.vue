27843c31a9972037b8db4b98490f151bab81e8ab
Oleg Nurzhanov <oleg@nastyle.pro>
2024-12-05 19:04:27 +0300

    update

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,9 +7,6 @@
             </a>
             <div class="flex gap-8 md:order-2">
                 <DarkMode />
-                <!-- <a href="#" @click="toggleTheme">ferfgregregre</a> -->
-                <!-- <button @click="toggleTheme" type="button">Лимпопо</button> -->
-                <!-- Поиск -->
                 <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
                     <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
@@ -23,7 +20,7 @@
                         </svg>
                         <span class="sr-only">Search icon</span>
                     </div>
-                    <input type="text" id="search-navbar" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Поиск...">
+                    <input @focus="goToSearch" type="search" id="search-navbar" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Поиск...">
                 </div>
                 <button data-collapse-toggle="navbar-search" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
@@ -41,7 +38,7 @@
                                 d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                         </svg>
                     </div>
-                    <input type="text" id="search-navbar"
+                    <input @focus="goToSearch" type="search" id="search-navbar"
                         class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="Поиск...">
                 </div>
@@ -81,5 +78,9 @@ const isActive = (path) => route.path.split('/')[1] === path
 
 function toggleTheme() {
     console.log('fefefwe');
+}
+
+function goToSearch() {
+    router.push('/search')
 }
 </script>
---
+++ pages/search.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Поиск</h1>
+</template>
+
+<script setup>
+const stringSearch = ref('')
+const response = await $fetch(`https://a1c537287dd6.vps.myjino.ru/api/posts?filters[$or][0][title][$containsi]=${stringSearch}&filters[$or][1][body][$containsi]=${stringSearch}`)
+const search = response.data
+
+</script>
