dafaa7670eb854adeac8fa0dcf72f40515cbdaf2
Oleg Nurzhanov <oleg@nastyle.pro>
2024-12-05 21:00:24 +0300

    fe

--- pages/search.vue
+++ pages/search.vue
@@ -11,8 +11,7 @@ const search = useSearchStore()
 
 function searcAlert() {
     console.log(search.searchQuery);
-    console.log(searchResponse);
-}
+} 
 
 // const response = await $fetch(`https://a1c537287dd6.vps.myjino.ru/api/posts?filters[$or][0][title][$containsi]=${search.searchQuery}&filters[$or][1][body][$containsi]=${search.searchQuery}`)
 // const searchResponse = response.data
