cd45be8fb5978045ce90911ec1ea3d8a14410325
Oleg Nurzhanov <oleg@nastyle.pro>
2024-12-05 21:05:46 +0300

    *

--- pages/search.vue
+++ pages/search.vue
@@ -2,27 +2,26 @@
     <h1>Поиск</h1>
 
     <main>
-        <button @click="searchAlert">Показать Search</button>
+        <article v-for="post in posts" :key="post.id">
+            <h3>{{ post.title }}</h3>
+        </article>
     </main>
 </template>
 
 <script setup>
 const search = useSearchStore()
-
-
-
-// const response = await $fetch(`https://a1c537287dd6.vps.myjino.ru/api/posts?filters[$or][0][title][$containsi]=${search.searchQuery}&filters[$or][1][body][$containsi]=${search.searchQuery}`)
-// const searchResponse = response.data
+const posts = ref()
 
 async function searchAlert() {
     console.log(search.searchQuery);
 
     try {
         const response = await $fetch(`https://a1c537287dd6.vps.myjino.ru/api/posts?filters[$or][0][title][$containsi]=${search.searchQuery}&filters[$or][1][body][$containsi]=${search.searchQuery}`);
-        const searchResponse = response.data;
-        console.log(searchResponse);
+        posts = response.data;
     } catch (error) {
         console.error('Ошибка при выполнении запроса:', error);
     }
 }
+
+onMounted(() => searchAlert())
 </script>
