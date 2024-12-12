fd036f5099b68c7e10be00834e65a2582de044f5
Oleg Nurzhanov <oleg@nastyle.pro>
2024-12-12 21:28:05 +0300

    update

--- app.vue
+++ app.vue
@@ -11,6 +11,9 @@
   const api = await $fetch(`${base}/api/config?populate=*`)
   const config = api.data
 
+  const isDarkMode = ref('')
+  const isDark = ref(false)
+
   useHead({
     title: config.title,
     meta: [
@@ -18,15 +21,45 @@
       { name: 'keywords', content: config.keywords },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: base+config.favicon.url },
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: base+config.favicon.url
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css',
+      },
     ],
+    htmlAttrs: {
+        class: isDarkMode.value
+    },
     bodyAttrs: {
       class: 'container mx-auto bg-white dark:bg-gray-900'
     },
     head: {
       script: [
-        { src: 'https://yastatic.net/share2/share.js', async: true, defer: true }
+        {
+          src: 'https://yastatic.net/share2/share.js',
+          async: true,
+          defer: true
+        },
+        {
+          src: 'https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js',
+          async: true,
+          defer: true
+        }
       ]
     }
   })
+
+onMounted(() => {
+  if (localStorage.getItem('color-theme') === 'dark') {
+      isDark.value = true;
+      isDarkMode.value = 'dark'
+  } else {
+      isDark.value = false;
+      isDarkMode.value = ''
+  }
+});
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,8 @@ export default defineNuxtConfig({
   },
 
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
-
   postcss: {
     plugins: {
       tailwindcss: {},
